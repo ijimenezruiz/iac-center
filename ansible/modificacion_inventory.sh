b84be658b034b730ae3a45f8ed0ef2c662b7bb9b
Ivan Jimenez <iv.jimenez.ruiz@gmail.com>
2020-04-10 21:37:24 +0200

    victoria sobre dhcp

--- ansible/modificacion_inventory.sh
+++ ansible/modificacion_inventory.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 sudo sed -i '/#inventory      =/c\inventory      = $OS_INVENTORY_PATH' /etc/ansible/ansible.cfg
-export OS_INVENTORY_PATH=$(pwd)/inventario
+sudo sed -i '/roles_path    =/c\roles_path    = $OS_ROLES_PATH' /etc/ansible/ansible.cfg
 
+export OS_INVENTORY_PATH=$(pwd)/inventario
+export OS_ROLES_PATH=$(pwd)/roles/
